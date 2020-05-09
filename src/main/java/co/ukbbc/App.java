55825335ef994ed8e45f78ee8ee037c373b023eb
shaikhsqa <53021592+shaikhsqa@users.noreply.github.com>
2020-05-09 19:29:09 +0100

    inital commit from windows commandline on 09/05/2020 at 7:29pm

---
+++ src/main/java/co/ukbbc/App.java
@@ -0,0 +1,13 @@
+package co.ukbbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
